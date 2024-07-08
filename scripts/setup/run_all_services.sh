274d9bd06bd42dd08a6249f534e5118f02e55724
skipandsnow <skipandsnow@gmail.com>
2024-07-08 22:43:13 +0800

    add services

--- scripts/setup/run_all_services.sh
+++ scripts/setup/run_all_services.sh
@@ -6,5 +6,8 @@ docker run -d -p 6333:6333 qdrant/qdrant
 # Run postgres
 docker run --name postgres -d -e POSTGRES_PASSWORD=llm -e POSTGRES_USER=llm postgres:16.3
 
+# Run ollama
+docker run -d --gpus=all -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
+
 # Run openldap
 docker run -d -p 6333:6333 qdrant/qdrant
