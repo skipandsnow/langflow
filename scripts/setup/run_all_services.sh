392939267bf265bf057118a1ca8e24c65612bf45
skipandsnow <skipandsnow@gmail.com>
2024-07-10 16:24:59 +0800

    WIP

--- scripts/setup/run_all_services.sh
+++ scripts/setup/run_all_services.sh
@@ -4,7 +4,7 @@
 docker run -d -p 6333:6333 qdrant/qdrant
 
 # Run postgres
-docker run --name postgres -d -e POSTGRES_PASSWORD=llm -e POSTGRES_USER=llm postgres:16.3
+docker run --name postgres -d -p 5432:5432 -e POSTGRES_PASSWORD=llm -e POSTGRES_USER=llm postgres:16.3
 
 # Run ollama
 docker run -d --gpus=all -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
