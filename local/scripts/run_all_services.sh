b6548da17d8b3172c9640ce923b0c00358fd6806
skipandsnow <skipandsnow@gmail.com>
2024-07-24 18:57:28 +0000

    update the ollama version

--- local/scripts/run_all_services.sh
+++ local/scripts/run_all_services.sh
@@ -9,8 +9,8 @@ docker run -d -p 6333:6333 qdrant/qdrant:v1.10.1
 docker run --name postgres -d -p 5432:5432 -e POSTGRES_PASSWORD=llm -e POSTGRES_USER=llm postgres:16.3
 
 # Run ollama
-docker pull ollama/ollama:0.2.5
-docker run -d --gpus=all -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama:0.2.5
+docker pull ollama/ollama:0.2.8
+docker run -d --gpus=all -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama:0.2.8
 
 # Run openldap
 docker run -p 1389:1389 -d --rm --name openldap \
