5f2483603b596c3e39b3b0d2847503c69b333673
skipandsnow <skipandsnow@gmail.com>
2024-08-18 10:23:28 +0800

    merge-20240818-v1.0.15-final

--- local/scripts/build_langflow_builder_base.sh
+++ local/scripts/build_langflow_builder_base.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Build langflow builder_base
-docker build --no-cache -t langflow-builder-base:1.0.12 -f local/docker/build_langflow_builder_base.Dockerfile .
+docker build --no-cache -t langflow-builder-base:1.0.15 -f local/docker/build_langflow_builder_base.Dockerfile .
